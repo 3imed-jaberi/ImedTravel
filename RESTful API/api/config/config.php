e5a4891df470a0a33a7e6d43d3a02279dbad029f
unknown <imed_jebari@hotmail.fr>
2019-04-20 22:06:36 +0200

    setup the client api ..

---
+++ RESTful API/api/config/config.php
@@ -0,0 +1,22 @@
+<?php 
+/*   // My MVC Framework .. 
+  define('IMED_VERSION', '0.0.1');
+ */
+  
+  // DB params // replace with formel data 
+  define('DB_HOST','localhost');
+  define('DB_USER','root');
+  define('DB_PASSWORD','22413578');
+  define('DB_NAME','imed');
+  
+
+  // EMAIL 
+  define('ADMIN_EMAIL','imed_jebari@hotmail.fr');
+  
+/*   // App Root 
+  define('APP_ROOT',dirname(dirname(__FILE__)));  
+  // Url Root
+  define('URL_ROOT','http://localhost/RESTful%20API/');
+  // Site Name 
+  define('SITE_NAME', 'PHP mini ');
+ */
