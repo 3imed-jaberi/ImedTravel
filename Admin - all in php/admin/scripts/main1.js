dd81266251afbe9e6764bb0adc64496366777447
unknown <imed_jebari@hotmail.fr>
2019-04-20 21:58:40 +0200

    jQuery scripts ...

---
+++ Admin - all in php/admin/scripts/main1.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+let edit = new URL(window.location.href).searchParams.get("edit");
+  if ( edit === 'true' ){
+    $('#dasboard').hide();
+    $("#printFrom").hide();
+    $('#add_update_forms').show();
+  }else{
+    $('#dasboard').show();
+    $("#printFrom").hide();
+    $('#add_update_forms').hide();
+
+    $('#add_btn').click(function(){
+      $('#dasboard').hide();
+      $('#add_update_forms').show();
+    });
+  }
+
+  $('#money_btn').click(function(){
+    $("#msg").show();
+    $.get("http://localhost/admin/crud%20api/sum.php", function(data, status){
+     // console.log("Data: " + data + "\nStatus: " + status);
+      $("#msg").text(" all money received : "+data);
+      setTimeout(function(){
+        $("#msg").hide();
+      },2000);
+    });
+  });
+
+
+  $('#print_btn').click(function(){
+    $("#printFrom").show();
+    setTimeout(function(){
+      $("#printFrom").hide();
+    },10000);
+  });
+
+})
