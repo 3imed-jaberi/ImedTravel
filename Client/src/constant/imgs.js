240ce6aad9faed8d75666b1f63f8f380e08f6a2d
3imed-jaberi <imed_jebari@hotmail.fr>
2020-03-19 17:23:45 +0100

    add const dir 🎼 ..

---
+++ Client/src/constant/imgs.js
@@ -0,0 +1,14 @@
+import logo from '../assets/images/imed jaberi/logo.jpg';
+import resort1 from '../assets/images/slider & popular places/resort1.jpg';
+import resort2 from '../assets/images/slider & popular places/resort2.jpg';
+import resort3 from '../assets/images/slider & popular places/resort3.jpg';
+import demo from "../assets/videos/Demo-Tunisia-Trip.mp4";
+
+
+export { 
+  logo as ChatBotLogo,
+  resort1,
+  resort2,
+  resort3,
+  demo
+}
