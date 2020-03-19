0987232393628d90fa5c395874a09e30d6bf1442
3imed-jaberi <imed_jebari@hotmail.fr>
2020-03-19 19:20:56 +0100

    root 😁 ..

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
