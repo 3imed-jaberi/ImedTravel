eaa72fb10250bf8ce60d1fa95267765ee84fb9b4
3imed-jaberi <imed_jebari@hotmail.fr>
2020-03-19 18:41:21 +0100

    update const dir 🎼 ..

--- Client/src/constant/imgs.js
+++ Client/src/constant/imgs.js
@@ -3,6 +3,12 @@ import resort1 from '../assets/images/slider & popular places/resort1.jpg';
 import resort2 from '../assets/images/slider & popular places/resort2.jpg';
 import resort3 from '../assets/images/slider & popular places/resort3.jpg';
 import demo from "../assets/videos/Demo-Tunisia-Trip.mp4";
+import errorIMG from '../assets/images/select forms/alert.png';
+import twoIMG from '../assets/images/select forms/two.png';
+import threeIMG from '../assets/images/select forms/three.png';
+import fourIMG from '../assets/images/select forms/four.png';
+import fiveIMG from '../assets/images/select forms/five.png';
+import notFound from '../assets/images/not found/404.jpg';
 
 
 export { 
@@ -10,5 +16,11 @@ export {
   resort1,
   resort2,
   resort3,
-  demo
+  demo,
+  errorIMG,
+  twoIMG,
+  threeIMG,
+  fourIMG,
+  fiveIMG,
+  notFound
 }
